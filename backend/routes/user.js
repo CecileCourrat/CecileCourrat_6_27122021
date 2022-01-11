1ce7d2d852164dacfc0f0854cc04e6b0114d1ee1
Cecile <cecile.courrat@gmail.com>
2022-01-11 14:09:31 +0100

    securité password

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,11 +2,13 @@ const express = require('express');
 //Création du routeur
 const router = express.Router();
 
+const password = require('../middleware/password');
+
 const limiter = require('../middleware/limiter');
 
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', password, userCtrl.signup);
 router.post('/login', limiter.loginLimiter, userCtrl.login);
 
 
